3ddeda93dbc93a0436d4427fe6c39d6e868a3f28
buihong17203 <138987421+buihong17203@users.noreply.github.com>
2023-08-06 10:10:38 +0700

    Add files via upload

--- QLSTV.cpp
+++ QLSTV.cpp
@@ -4,78 +4,29 @@
 #include"iomanip"
 #include"string.h"
 using namespace std;
- 
-struct book
+
+struct Sach
 {
-	int MaS;
-	string TenTG , TenSach;
-	double Sachtrongkho , Sachngoaikho , Tongsach;
+// MaS , TenS , TenTacGia , NgayNhap , LoaiSach , SoTrang , TenNXB , MaNXB , GiaSach , SoLuongSachTrongKho , SoLuongSachNgoaiKho , TongSoLuongSach 
+	int MaS , MaNXB ,SoTrang , GiaSach ; 
+	string TenTacGia , LoaiSach , TenS , TenNXB ;
+	double SoLuongSachTrongKho , SoLuongSachNgoaiKho , TongSoLuongSach;
 };
 
-book bo[1000];
+Sach bo[10000]
 
-//Ham nhap thong tin cho sach
-void nhap(book bo[],int n)
+//ham nhap du lieu cho struct sach
+void nhap(Sach bo[],int n)
 {
-	for(int i=0;i<n;i++)
+	for(int i=0;i<=n;i++)
 	{
-		cout<<"\n Ten Sach "<<i+1<<": "<<endl;
-		
-		cout<<"\n Ma Sach "<<i+1<<": ";cin>>bo[i].MaS;
-			fflush(stdin); //xoa bo ho dem
-		cout<<"\n Ten Tac Gia: ";getline(std::cin,bo[i].TenTG);
-		
-		cout<<"\n So Sach Trong Kho: ";cin>>bo[i].Sachtrongkho;
-		
-		cout<<"\n So Sach Ngoai Kho: ";cin>>bo[i].Sachngoaikho;
-		bo[i].Tongsach = (bo[i].Sachtrongkho + bo[i].Sachngoaikho);
-		cout<<"\n Tong So Sach: ";cin>>bo[i].Tongsach;
-	}	
-}
-
-//xuat danh sach 
-void xuat(book bo[],int n)
-{
-	cout<<"\MaSach:";
-	cout<<setw(50)<<"TenSach"<<setw(50)<<"TenTG";
-	cout<<setw(50)<<"Sachtrongkho"<<setw(10)<<"Sachngoaikho";
-	cout<<setw(50)<<"Tongsach";
-	
-	for(int i=0;i<n;i++)
-	{
-		cout<<""<<setw(50)<<bo[i].MaS<<setw(50)<<bo[i].TenSach;
-		cout<<""<<setw(50)<<bo[i].TenTG;
-		cout<<""<<setw(50)<<bo[i].Sachtrongkho<<setw(50)<<bo[i].Sachngoaikho<<setw(50)<<bo[i].Tongsach<<endl;
+		cout<<"Ma Sach "<<i+1<<": ";cin>>bo[i].MaS<<endl;
+		cout<<"Ten Sach: ";
+		cout<<
 	}
 }
-
+	
 int main()
 {
-	int option,n;
-	cout<<"\n Nhap so luong Sach: ";
-	cin>>n;
-	while(n>0)
-	{
-		
-	cin>>option;
-	switch(option)
-		{
-		case 1:
-			cout<<"* Moi nhap thong tin Sach: \n";
-			nhap(bo,n);
-			cout<<"Bam phim bat ki de tiep tuc: ";
-			system("pause");
-			break;
-		case 2:
-			if(n>0)
-			cout<<"Danh sach Sach: \n";
-			xuat(bo,n);
-			cout<<"Bam phim bat ki de tiep tuc: ";
-			system("pause");
-			break;
-				
-		default:
-		return 0;
-		}
-	}
+	
 }
